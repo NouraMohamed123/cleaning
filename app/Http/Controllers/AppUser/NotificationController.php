fa2e3f9e1e2c7521f7e5b0b945a5c34b8de0e36e
NouraMohamed123 <nouramohamed091@gmail.com>
2024-03-12 01:43:01 +0200

    yy

--- app/Http/Controllers/AppUser/NotificationController.php
+++ app/Http/Controllers/AppUser/NotificationController.php
@@ -9,20 +9,20 @@ use Illuminate\Support\Facades\Auth;
 class NotificationController extends Controller
 {
     public function NotificationRead(){
-        if(Auth::guard('app_users')->user()->notifications){
+        if(!empty(Auth::guard('app_users')->user()->notifications)){
         $notifications = Auth::guard('app_users')->user()->notifications;
         }
         return response()->json(['isSuccess' => true,'data'=> $notifications ], 200);
     }
     public function MarkASRead(){
-        if(Auth::guard('app_users')->user()->notifications){
+        if(!empty(Auth::guard('app_users')->user()->notifications)){
      $notifications  =   Auth::guard('app_users')->user()->notifications->markAsRead();
         return response()->json(['isSuccess' => true], 200);
         }
         return response()->json(['isSuccess' => false], 200);
     }
     public function Clear(){
-        if(Auth::guard('app_users')->user()->notifications){
+        if(!empty(Auth::guard('app_users')->user()->notifications)){
         $notifications  =   Auth::guard('app_users')->user()->notifications()->delete();
         return response()->json(['isSuccess' => true,'data'=> $notifications ], 200);
         }
