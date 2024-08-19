dc1eed3254ef9d3364efcef424c4b90539122a90
NouraMohamed123 <nouramohamed091@gmail.com>
2024-08-19 19:27:03 +0300

    شيي

--- app/Http/Requests/CouponRequest.php
+++ app/Http/Requests/CouponRequest.php
@@ -27,8 +27,8 @@ class CouponRequest extends FormRequest
             'start_date' => 'required|date',
             'end_date' => 'required|date|after:start_date',
            // 'discount' => 'required|numeric',
-            'max_usage' => 'required|integer',
-            'max_discount_value' => 'required|numeric',
+            // 'max_usage' => 'required|integer',
+            // 'max_discount_value' => 'required|numeric',
 
         ];
     }
