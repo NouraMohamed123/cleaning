5245d7b54aec8f6f3ec073e91c00c1bff8d1297e
NouraMohamed123 <nouramohamed091@gmail.com>
2024-08-26 21:16:01 +0300

    ff

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -15,7 +15,7 @@ class ServiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return array_merge(parent::toArray($request), [
-            'category' => $this->category->name,
+
             'photo' =>  asset('uploads/service_photo/' . $this->photo),
 
         ]);
