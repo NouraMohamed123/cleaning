d574695e53ba71896bd734d72aa47246bbe1bfa7
unknown <nouramohamed091@gmail.com>
2024-10-02 13:09:55 +0200

    fix

--- app/Http/Controllers/Admin/SubscriptionController.php
+++ app/Http/Controllers/Admin/SubscriptionController.php
@@ -74,7 +74,7 @@ class SubscriptionController extends Controller
                 'duration' => $request->duration,
                 'status' => $request->status ? 'active' : 'inactive',
                 'photo' => $photo, // Save the correct photo path
-                  'details'=>$request->details
+                'details'=>$request->details
             ]);
     
            
@@ -149,7 +149,7 @@ class SubscriptionController extends Controller
             'price_offer' => $request->price_offer,
             'duration' => $request->duration,
             'status' => $request->status,
-           'photo' => $request->photo,
+           'photo' =>  $photo,
            'details'=>$request->details
         ]);
 
